847ef01a2c232ac0d36827714efa21efb1410801
AnnaFiedler <anna.fiedler@helmholtz-muenchen.de>
2018-08-21 19:36:02 +0200

    extract file for new population size data

--- Qiu/extractDataQiu.m
+++ Qiu/extractDataQiu.m
@@ -1,31 +1,31 @@
 %% area weighted cdf
-R=readtable('./rawData/pseudodynamics_input_qiu17.csv');
-RN=readtable('./rawData/pseudodynamics_input_qiu17_populationsize_imputed0.csv','TreatAsEmpty','NA');
+R=readtable('./rawData_new/pseudodynamics_input_qiu17.csv');
+RN=readtable('./rawData_new/pseudodynamics_input_qiu17_populationsize.csv','TreatAsEmpty','NA');
 
 % number of replicates for population size
-n = RN{1:end-2,4};
+n = RN{:,4};
 
 % population information
-D.pop.t = RN{1:end-2,1};
+D.pop.t = RN{:,1};
 
 % log population
 % 1st data point for initializing linear, 2nd-9th log
-D.pop.mean = RN{1:end-2,6};
-D.pop.var = RN{1:end-2,5}.^2;
+D.pop.mean = RN{:,6};
+D.pop.var = RN{:,5}.^2;
 
 % scaling to 0.9 
-R{:,2} = R{:,2}*0.9/max(R{:,2});
+R{:,4} = R{:,4}*0.9/max(R{:,4});
 
-Rbatch = R{:,1};
+Rbatch = R{:,3};
 [Rubatch,ia,ic] = unique(Rbatch);
-tp = R{ia,10};
+tp = R{ia,2};
 [tp,indT]=sort(tp,'ascend');
 Rubatch = Rubatch(indT);
-D_tmp(:,1) = R{:,2};
+D_tmp(:,1) = R{:,4};
 
 for iL = 1:length(Rubatch)
-    ind{iL} = find(strcmp(R{:,1},Rubatch(iL))==1);
-    D.ind.tp(iL) = R{ind{iL}(1),10};
+    ind{iL} = find(strcmp(R{:,3},Rubatch(iL))==1);
+    D.ind.tp(iL) = R{ind{iL}(1),2};
     D.ind.hist{iL} = D_tmp(ind{iL});
     D.ind.size(iL) = length(ind{iL});
 end
@@ -40,4 +40,4 @@ for i = 1: length(D.ind.hist)
     D.xsdt{i} = xsd{i}(2:end);
 end
 
-save('dataQiu_logN_imputed0','D');
+save('dataQiu_logN','D');
