2c8d1f97b1fd363f47786c3788a5bb41b1f1c883
AnnaFiedler <anna.fiedler@helmholtz-muenchen.de>
2018-08-22 14:54:23 +0200

    Add files via upload

---
+++ SimulatedData/llPseudodynamicsFvKS_wBcorr.m
@@ -0,0 +1,111 @@
+function [LogL,grad] = llPseudodynamicsFvKS_wBcorr(varargin)
+% compute log-likelihood for pseudodynamics with bias correction for linear
+% bias
+
+if nargin >= 3
+	theta = varargin{1};
+	modelFun = varargin{2};
+	D = varargin{3};
+else
+	error('Not enough inputs!');
+end
+
+n_grid = 300;
+% default options
+options.grid = linspace(0,1,n_grid);
+options.alpha = 100;
+options.n_exp = 1;
+if nargin == 4
+	options = setdefault(varargin{4},options);
+end
+
+% u0 approximated by kernel density estimate
+if ~isfield(options,'u0')
+    for i0 = 1:3
+        u0(i0,:) = ksdensity(D.ind.hist{i0},x,'support',[0,1],'function','pdf');
+        N01(i0) = length(D.ind.hist{i0});
+        u0(i0,:) = u0(i0,:)/trapz(x,u0(i0,:));
+    end
+    u0 = mean(u0,1)*D.pop.mean(1);
+    u0 = 0.5*(u0(1:n_grid-1)+u0(2:n_grid));
+else
+    u0 = options.u0;
+end
+
+options_sim.sensi = 1;
+options_sim.maxsteps = 1e6;
+options_sim.sx0 = zeros(n_grid-1,length(theta));
+% simulation
+[status,t_sim,~,u,~,us] = modelFun(D.pop.t,theta,u0,[],options_sim);
+% check if simulation failed
+if status~=0    
+    error('AMICI simulation failed')
+end
+
+% variable assignment
+N = u(:,end);
+dNdtheta = us(:,end,:);
+
+u = u(:,1:n_grid-1);
+dudtheta = us(:,1:n_grid-1,:);
+
+% computation of probability
+pb = bsxfun(@rdivide,u,N)*(1/(n_grid-1));
+dpbdtheta = 1/(n_grid-1)*bsxfun(@rdivide,bsxfun(@times,N,dudtheta)-...
+    bsxfun(@times,u,dNdtheta),N.^2);
+
+% compute biased probability
+grid_x = linspace(options.grid(1)+(options.grid(2)-options.grid(1))/2,...
+    options.grid(end-1)+(options.grid(end)-options.grid(end-1))/2,n_grid-1);
+bias = options.a*grid_x+options.b;
+pb = bsxfun(@times,pb,bias);
+dpbdtheta = bsxfun(@times,dpbdtheta,bias);
+% normalization
+sp = sum(pb,2);
+dspdtheta = sum(dpbdtheta,2);
+p = bsxfun(@rdivide,pb,sp);
+dpdtheta = bsxfun(@rdivide,bsxfun(@times,sp,dpbdtheta)-...
+    bsxfun(@times,pb,dspdtheta),sp.^2);
+
+% compute biased cdf
+cs = cumsum(p');
+dcsdtheta = cumsum(dpdtheta,2);
+
+% log-likelihood
+LogL = 0;
+grad = zeros(size(theta));
+
+area = nan(length(D.pop.t)-1,1);
+dareadtheta = nan(length(D.pop.t)-1,length(theta));
+
+% area statistics
+for it = 2:length(D.pop.t)
+    x_combined = options.x_combined{it};
+    Aug_matrix = options.Aug_matrix{it};
+    cs_a = Aug_matrix*cs(:,it);
+    dcs_adtheta = Aug_matrix*squeeze(dcsdtheta(it,:,:));
+    csd_a = D.csd_a{it};
+    area(it) = sum(diff(x_combined).*abs(cs_a(1:end-1)-csd_a(1:end-1)));
+    dareadtheta(it,:) = (diff(x_combined).*sign(cs_a(1:end-1)-...
+        csd_a(1:end-1)))'*(dcs_adtheta(1:end-1,:));
+    LogL = LogL + 0.5*log(2*pi*D.dist.var(it)) +...
+        0.5*(D.dist.mean(it)-area(it)).^2/D.dist.var(it);
+    grad = grad - ((D.dist.mean(it)-area(it))/D.dist.var(it)*dareadtheta(it,:))';
+end
+
+% population level statistics
+for it = 2:length(D.pop.t)
+    LogL = LogL + 0.5*log(2*pi*D.pop.var(it)/options.n_exp) +...
+        0.5*(D.pop.mean(it)-N(it)).^2/(D.pop.var(it)/options.n_exp);
+    grad = grad  - ((D.pop.mean(it)-N(it))/(D.pop.var(it)/options.n_exp)*...
+        squeeze(dNdtheta(it,:,:))')';
+end
+
+% regularization
+LogL = LogL + options.alpha*(sum((theta(2:9)-theta(1:8)).^2)+...
+    sum((theta(11:18)-theta(10:17)).^2)+sum((theta(20:27)-theta(19:26)).^2));
+grad = grad + options.alpha*2*([-(theta(2:9)-theta(1:8));0;...
+    -(theta(11:18)-theta(10:17));0;-(theta(20:27)-theta(19:26));0]...
+    +[0;theta(2:9)-theta(1:8);0;theta(11:18)-theta(10:17);0;...
+    theta(20:27)-theta(19:26)]);
+end
